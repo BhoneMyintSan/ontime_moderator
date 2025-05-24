939ca47aa927c4a48913efc2667d306d27f3b41f
BhoneMyintSan <u6520225@au,edu>
2025-05-24 12:11:32 +0700

    initial commit

--- app/page.js
+++ app/page.js
@@ -26,10 +26,3 @@ export default function Home() {
   );
 }
 
-export function DashboardHome() {
-  return (
-    <div className="text-2xl sm:text-3xl font-bold text-center mt-32 sm:mt-40 px-2">
-      Welcome to the OnTime Moderator Panel
-    </div>
-  );
-}
